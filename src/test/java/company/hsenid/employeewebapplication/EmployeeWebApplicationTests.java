71b4a9389711397b8e104577f49b9343ebeecfa2
CoderX02 <rithika.h@hsenidoutsourcing.com>
2022-05-23 14:10:46 +0530

    -First Commit-

---
+++ src/test/java/company/hsenid/employeewebapplication/EmployeeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package company.hsenid.employeewebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

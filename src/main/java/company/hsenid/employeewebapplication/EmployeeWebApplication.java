a4b7ce48808e632ba24b7003dc210e5412843624
CoderX02 <rithika.h@hsenidoutsourcing.com>
2022-05-27 09:44:28 +0530

    -swagger implementation-

--- src/main/java/company/hsenid/employeewebapplication/EmployeeWebApplication.java
+++ src/main/java/company/hsenid/employeewebapplication/EmployeeWebApplication.java
@@ -3,8 +3,12 @@ package company.hsenid.employeewebapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableWebMvc
 public class EmployeeWebApplication {
 
     public static void main(String[] args) {
